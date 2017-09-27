a9702e682e1fb622f04df1deaaf8b6bc4d0f4f86
teng <teng>
2017-09-27 15:10:19 +0800

    'luka'

--- src/components/MeetUp/Meetups.vue
+++ src/components/MeetUp/Meetups.vue
@@ -1,8 +1,24 @@
 <template>
-  <v-contianer>
-    <v-layout>
-      <v-flex>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card class="info">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs5 sm4 md3>
+                <v-card-media
+                  src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1505478282424&di=df32488b494839f4cb6a3c45bbdde4dd&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3D7ffa26dc33dbb6fd3156ed65614dc16d%2F5366d0160924ab18b90b73103ffae6cd7b890bf3.jpg"
+                  height="130px">
+                </v-card-media>
+              </v-flex>
+              <v-flex xs7 sm8 md9>
+                <v-card-title>
+                </v-card-title>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
       </v-flex>
     </v-layout>
-  </v-contianer>
+  </v-container>
 </template>
